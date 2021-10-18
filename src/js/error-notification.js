fd6ebe6031b72f73e666ea7b11a82813a7a4bf1a
Ivan Suprun <bender.slam@gmail.com>
2021-10-18 17:02:00 +0300

    Summary

---
+++ src/js/error-notification.js
@@ -0,0 +1,10 @@
+import PNotify from '../../node_modules/pnotify/dist/es/PNotify.js';
+import PNotifyButtons from '../../node_modules/pnotify/dist/es/PNotifyButtons.js';
+import '../../node_modules/pnotify/dist/PNotifyBrightTheme.css';
+
+export default function showError(errorMessage) {
+  PNotify.error({
+    text: errorMessage,
+    delay: 4000,
+  });
+}
